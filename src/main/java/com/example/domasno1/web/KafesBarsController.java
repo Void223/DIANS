30545ca576c018ce29155c0d2b58388507e91d8a
Andrej Kocev <andrejkocev1998@gmail.com>
2022-12-12 22:31:47 +0100

    Added controller for the app

--- src/main/java/com/example/domasno1/web/KafesBarsController.java
+++ src/main/java/com/example/domasno1/web/KafesBarsController.java
@@ -1,8 +1,34 @@
 package com.example.domasno1.web;
 
+import com.example.domasno1.Model.Cocktails;
+import com.example.domasno1.Model.KafesBars;
+import com.example.domasno1.Service.CocktailsService;
+import com.example.domasno1.Service.KafesBarsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class KafesBarsController {
 
+    private final CocktailsService cocktailsService;
+    private final KafesBarsService kafesBarsService;
+
+    public KafesBarsController(CocktailsService cocktailsService, KafesBarsService kafesBarsService){
+        this.cocktailsService = cocktailsService;
+        this.kafesBarsService = kafesBarsService;
+    }
+
+    @GetMapping({"/kafebars"})
+    public String showKafeBars(){
+        List<KafesBars> kafesBarsList;
+        kafesBarsList = kafesBarsService.listAll();
+
+    }
+
+    public String showCocktails(){
+        List<Cocktails> cocktailsList;
+        cocktailsList = cocktailsService.listAll();
+    }
 }
